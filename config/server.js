93af7ef1d027368bb251be4da8573ce4114ac86b
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-03-13 08:49:12 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '85fee0b2c3155059e3d6346a2943deb2'),
+    },
+  },
+});
