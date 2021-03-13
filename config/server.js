1fcb65eb79869e166df864489489b7b69ce9987e
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-03-13 14:10:07 +0100

    update server config

--- config/server.js
+++ config/server.js
@@ -1,10 +1,13 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://api.bifolia.de'),
   admin: {
-    url: '/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '85fee0b2c3155059e3d6346a2943deb2'),
     },
+    url: env('PUBLIC_ADMIN_URL', '/admin'),
+    autoOpen: true,
+    serveAdminPanel: env.bool('SERVE_ADMIN', true),
   },
 });
