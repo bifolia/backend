67959ec0685fd2c3d8687b65e0e2def83bb3a370
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-04-04 21:58:11 +0200

    upload media to cloudinary

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // url: env('PUBLIC_URL', 'https://api.bifolia.de'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '85fee0b2c3155059e3d6346a2943deb2'),
