622009b32f19bbd4f624b4ea8aee91e0b2da0a87
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-12-29 10:26:25 +0100

    change image provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,15 @@
 module.exports = ({env}) => ({
   upload: {
-    provider: 'cloudinary',
-    providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+    config: {
+      provider: "strapi-provider-upload-imagekit",
+      providerOptions: {
+        publicKey: env('IMAGEKIT_PUBLIC_KEY'),
+        privateKey: env('IMAGEKIT_PRIVATE_KEY'),
+        urlEndpoint: `https://ik.imagekit.io/${env('IMAGEKIT_ID')}`,
+        params: {
+          folder: "/bifolia"
+        }
+      }
     }
   }
 });
